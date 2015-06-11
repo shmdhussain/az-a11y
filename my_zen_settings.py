01146be9ab19d2ce26f2c802074c0b9442fbc807
mohamed_hussain01 <ms4360@att.com>
2015-06-11 22:06:04 +0530

    added few more elements

---
+++ my_zen_settings.py
@@ -0,0 +1,73 @@
+my_zen_settings = {
+     'html': {
+	 
+		
+      	'abbreviations': {
+			'jq': '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>',
+			'demo': '<div id   ="demo"></div>',
+			'yui':'<div class="ff"><a href="#">pp</a></div>',
+			'myclear':'<div class="clear">	</div>',
+			
+			},
+			'snippets':{
+			
+			# Web Accessibility Tags Start
+			
+			'aza':'<a id="optionalID" class="optionalClass"  title="optionalTitle" href="Product.html" >Product page</a>',
+			'aza-img':'<a id="optionalID" class="optionalClass"  title="optionalTitle" href="input.html" ><img src="images/Product.jpg" alt="Product page" /> </a>',
+			'azimg':'<img src="images/placeholder.jpg" alt="placeholderimage" /> ',
+			'azabbr':'<abbr title="British Broadcast Corporation">BBC</abbr>',
+			'azinput-text1':' <label for="fn">Name</label> <input id="fn" type="text" name="Firstname" value=""> ',
+			'azinput-text2':'<input type="text" title="site search" name="query" id="q" value="">',
+			'azinput-radio1':'<input id="yes" name="request" value="yes" type="radio"> <label for="yes">Yes</label>\n'+ 
+							'<input id="no" name="request" value="no" type="radio"> <label for="no">No</label>',
+			'azinput-radio2':'<input id="yes" name="request" value="yes" type="radio" title="yes"> \n'+ 
+							'<input id="no" name="request" value="no" type="radio" title="no"> ',				
+			'aztable1':'<table> \n'+ 
+					'		<caption>Cups of coffee consumed</caption>\n'+ 
+					'		<thead>\n'+ 
+					'			<tr>\n'+ 
+					'				<th id="header1">Name</th>\n'+ 
+					'				<th id="header2">Cups</th>\n'+ 
+					'			</tr>\n'+ 
+					'		</thead>\n'+ 
+					'		<tbody>\n'+ 
+					'			<tr>\n'+ 
+					'				<td headers="header1">Jack</td>\n'+ 
+					'				<td headers="header2">10</td>\n'+ 
+					'			</tr>\n'+ 
+					'		</tbody>\n'+ 
+					'	</table>',
+			'aztable2':	'<table>\n'+
+						'		<caption>Student List</caption>\n'+
+						'		<tr>\n'+
+						'			<th scope="col">Name</th>\n'+
+						'			<th scope="col">Age</th>\n'+
+						'			<th scope="col">Birthday</th>\n'+
+						'		</tr>\n'+
+						'		<tr>\n'+
+						'			<th scope="row">Jackie</th>\n'+
+						'			<td>5</td>\n'+
+						'			<td>April 5</td>\n'+
+						'		</tr>\n'+
+
+						'		<tr>\n'+
+						'			<th scope="row">Beth</th>\n'+
+						'			<td>8</td>\n'+
+						'			<td>January 14</td>\n'+
+						'		</tr>\n'+
+						'</table>',	
+			'azhtml':'<!doctype html>\n'+
+					'	<html lang="en">\n'+
+					'		<head>\n'+
+					'			<title>Accessibility Demo</title>\n'+
+					'			<meta charset="utf-8"/>\n'+
+					'		</head>\n'+
+					'		<body>\n'+
+					'		</body>\n'+
+					'	</html>'
+			
+			
+		}
+   }
+}
